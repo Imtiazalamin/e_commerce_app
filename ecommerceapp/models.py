3e57e9d31989caae0ff4d280c726e5bd40c4b414
Imtiazalamin <mda757637@gmail.com>
2025-07-22 23:53:23 +0600

    models make kora holo

---
+++ ecommerceapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+
+class product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2,)
+
+    
+
+
